08e7f8e5f2bda16274fd5d7edbd57c4d6a9d8097
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-08-06 11:15:13 +0700

    Add submit confirmation dialog

--- application/views/pembinaan/participant_attendance/add_attendance.php
+++ application/views/pembinaan/participant_attendance/add_attendance.php
@@ -9,12 +9,13 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="<?=site_url('dashboard')?>">Home</a></li>
+            <li class="breadcrumb-item"><a href="<?= site_url('dashboard') ?>">Home</a></li>
             <li class="breadcrumb-item active">Presensi Peserta</li>
           </ol>
         </div>
       </div>
-    </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.container-fluid -->
   </section>
 
   <!-- Main content -->
@@ -25,6 +26,11 @@
           <div class="card card-primary card-outline">
             <div class="card-header">
               <h3 class="card-title"><b>Presensi Peserta Pembinaan</b></h3>
+              <div class="float-sm-right">
+                <a href="<?= site_url('presensi') ?>" class="btn btn-warning">
+                  <b style="color: white"><i class="fas fa-chevron-left"></i> Back</b>
+                </a>
+              </div>
             </div>
             <!-- /.card-header -->
             <div class="card-body pad table-responsive">
@@ -32,21 +38,17 @@
                 <div class="form-group">
                   <label for="tglmulai">Tgl Presensi *</label>
                   <div class="input-group date" id="tglmulai" data-target-input="nearest">
-                    <input type="text" name="attendance_date" class="form-control datetimepicker-input" data-target="#tglmulai" required/>
+                    <input type="text" name="attendance_date" class="form-control datetimepicker-input <?= form_error('attendance_date') ? 'is-invalid' : ''; ?>" data-target="#tglmulai" value="<?= date('Y-m-d') ?>"/>
                     <div class="input-group-append" data-target="#tglmulai" data-toggle="datetimepicker">
                       <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                     </div>
+                    <div class="invalid-feedback"><?= form_error('attendance_date'); ?></div>
                   </div>
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-success">
-                      <b><i class="fas fa-paper-plane"> Save</i></b>
-                    </button>
-                  </div>
-                  <div class="float-sm-right">
-                    <a href="<?= site_url('presensi') ?>" class="btn btn-warning">
-                      <b style="color: white"><i class="fas fa-undo"></i> Back</b>
-                    </a>
-                  </div>
+                </div>
+                <div class="form-group float-sm-right">
+                  <button type="submit" id="saveButton" class="btn btn-success">
+                    <b><i class="fas fa-paper-plane"> Save</i></b>
+                  </button>
                 </div>
                 <table class="table table-bordered table-striped table-head-fixed">	
                   <thead>
@@ -93,3 +95,12 @@
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+<script>
+document.getElementById("saveButton").addEventListener("click", function(event) {
+  var confirmation = confirm("Apakah Anda yakin ingin menyimpan data presensi?");
+  if (!confirmation) {
+    event.preventDefault(); // Menghentikan proses submit form jika konfirmasi ditolak
+  }
+});
+</script>
